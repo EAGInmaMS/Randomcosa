b65a196f2fb4629d2847a04c64989ad725f5fe9f
EAGinmaMS <eaginma@gmail.com>
2022-03-31 13:33:37 +0200

    jamon

--- Radomcosa/src/radomcosa/Radomcosa.java
+++ Radomcosa/src/radomcosa/Radomcosa.java
@@ -6,6 +6,9 @@ public class Radomcosa {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Adiós mundo cruel");
+        System.out.println("Mayonesa");
+        System.out.println("Segundo día");
+        System.out.println("Desde Linux");
     }
     
 }
